5452f92a83182dee1cd56ba230e487edd771c007
jagat95 <jagatsingh_2008@yahoo.in>
2023-12-02 14:32:36 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,77 @@
+import os
+import re
+import sys
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+class FileHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        if event.is_directory:
+            return
+
+        file_path = event.src_path
+        organize_file(file_path)
+
+def organize_file(file_path):
+    filename = os.path.basename(file_path)
+    
+    # Extracting date/time and file name
+    date_time, file_name = extract_date_time_and_name(filename)
+    
+    if date_time and file_name:
+        folder_path = os.path.join(folder_to_write, file_name)
+
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+
+        new_file_path = os.path.join(folder_path, filename)
+        os.rename(file_path, new_file_path)
+        print(f"Moved {filename} to {file_name} folder")
+    else:
+        print(f"No match found for: {filename}")
+
+def extract_date_time_and_name(filename):
+    # Define a pattern to capture the date/time information
+    pattern = r'(\d{1,2}_\d{1,2}_\d{4} \d{1,2}_\d{1,2}_\d{1,2} [APMapm]+|\d{4}-\d{2}-\d{2} \d{2}-\d{2}-\d{2})'
+    
+    parts = re.split(pattern, filename, maxsplit=1)
+    
+    if len(parts) > 1:
+        file_name = parts[0].strip()
+        date_time = parts[1].strip()
+        return date_time, file_name
+    else:
+        return None, None
+
+def watch_folder(folder_path):
+    event_handler = FileHandler()
+    observer = Observer()
+    observer.schedule(event_handler, folder_path, recursive=False)
+    observer.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+
+    observer.join()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python script.py <folder_to_watch> <folder_to_write>")
+        sys.exit(1)
+
+    folder_to_watch = sys.argv[1]
+    folder_to_write = sys.argv[2]
+
+    if not os.path.exists(folder_to_watch) or not os.path.isdir(folder_to_watch):
+        print(f"Error: {folder_to_watch} is not a valid directory.")
+        sys.exit(1)
+
+    if not os.path.exists(folder_to_write) or not os.path.isdir(folder_to_write):
+        print(f"Error: {folder_to_write} is not a valid directory.")
+        sys.exit(1)
+
+    watch_folder(folder_to_watch)
